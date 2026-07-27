be5d24483d9e37b213e345c6f4a8e7a988c0dd49
Nguyen Tuan Anh <tuananh.reaction@gmail.com>
2026-07-27 14:30:02 +0700

    "Hoàn thành code Ngày 2: FormLogin, Utils, và Session"

---
+++ Utills/UserSession.cs
@@ -0,0 +1,10 @@
+﻿namespace LTWIN.Utils
+{
+    public static class UserSession
+    {
+        public static int UserId { get; set; }
+        public static string Username { get; set; }
+        public static string Role { get; set; }
+        // Role dùng để phân quyền Admin/Nhân viên ở Ngày 4
+    }
+}
